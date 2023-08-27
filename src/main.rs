20d61d1ccd84b5b79b74b99583a39406e8c1d672
Rick Hall <rickhall2000@hotmail.com>
2023-08-27 11:59:16 -0400

    game loop created

--- src/main.rs
+++ src/main.rs
@@ -3,14 +3,14 @@ use std::collections::HashMap;
 
 #[derive(Debug)]
 enum Question {
-    Leaf{name: String, id: i32},
+    Leaf{name: String, _id: i32},
     Branch{question: String, yes: i32, no: i32}
 }
 
 enum Result {
     Continue(i32),
     Win,
-    Lose(i32),
+    Lose,
 }
 
 fn get_answer() -> bool {
@@ -26,14 +26,14 @@ fn get_answer() -> bool {
 impl Question {
     fn ask(&self) -> Result {
         match self {
-            Question::Leaf{name, id} => {
+            Question::Leaf{name, _id: _} => {
                 println!("Is it a {name}");
                 if get_answer() {
                     println!("I win");
                     Result::Win
                 } else {
                     println!("You win");
-                    Result::Lose(*id)
+                    Result::Lose
                 }
             },
 
@@ -55,9 +55,9 @@ impl Question {
 fn initial_questions() -> HashMap<i32,Question>{
     let mut questions = HashMap::new();
 
-    let elephant = Question::Leaf {name: String::from("Elephant"), id: 2};
+    let elephant = Question::Leaf {name: String::from("Elephant"), _id: 2};
     let first = Question::Branch {question: String::from("Is it a mamal?"), yes: 2, no: 3};
-    let shark = Question::Leaf {name: String::from("Shark"), id: 3};
+    let shark = Question::Leaf {name: String::from("Shark"), _id: 3};
   
 
     questions.insert(1, first);
@@ -73,15 +73,57 @@ fn print_questions(questions: HashMap<i32,Question>)
     }
 }
 
+fn handle_loss() {
+    println!("What was your animal?");
+
+
+    let mut right_answer = String::new();
+
+    io::stdin() 
+    .read_line(&mut right_answer)
+    .expect("I want input");
+
+    println!("You said: {right_answer}");
+}
+
+fn play_game(questions: &HashMap<i32,Question>) {
+    let mut current_question = questions.get(&1).expect("This better work");
+    
+    loop {
+        let my_result = current_question.ask();
+
+        match my_result {
+            Result::Continue(next) => {
+                println!("I should continue with {next}");
+                current_question = questions.get(&next).expect("Lookup failed");
+            }, 
+            Result::Win => { 
+                println!("I am done"); 
+                break;
+            },
+            Result::Lose => {
+                handle_loss();
+                break;
+            }
+
+        };
+    }
+}
+
 fn main() {
 
     let questions = initial_questions();
+    
+    loop {
+        play_game(&questions);
+        let mut play_again = String::new();
+        println!("Would you like to play again?");
+        io::stdin()
+        .read_line(&mut play_again)
+        .expect("I expect readline to work");
 
-    let x = questions.get(&1).expect("This better work");
-    x.ask();
-
-    let x = questions.get(&2).expect("This better work");
-    x.ask();
+        if play_again.trim() != "yes" { break }
+    }
 
     print_questions(questions);
     println!("All done");
